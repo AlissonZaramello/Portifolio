dcf461b2cf44903257d1f6aaca1274ccea6af851
AlissonZaramello <alissonzaramello34@gmail.com>
2024-10-04 16:48:13 -0300

    Implementação do primeiro projeto

--- script/script.js
+++ script/script.js
@@ -1,6 +1,11 @@
 let btnMenu = document.getElementById('btn-menu');
 let menu = document.getElementById('menu-mobile');
 let overlay = document.getElementById('overlay-menu');
+let setaCima = document.getElementById('btn-topo');
+
+let popup = document.getElementById('popupProjNapne');
+let fecharPopup = document.getElementById('fechar-popup');
+let projeto = document.getElementById('NAPNE');
 
 btnMenu.addEventListener('click', ()=>{
     menu.classList.add('abrir-menu');
@@ -12,4 +17,25 @@ menu.addEventListener('click', ()=>{
 
 overlay.addEventListener('click', ()=>{
     menu.classList.remove('abrir-menu');
+});
+
+fecharPopup.addEventListener('click', ()=>{
+    popup.style.display = "none";
+})
+
+projeto.addEventListener('click', ()=>{
+    popup.style.display = "flex";
+});
+
+BtnSetaCima = () => {
+    if(window.scrollY >= 300){
+        setaCima.classList.add('show');
+    }
+    else{
+        setaCima.classList.remove('show');
+    }
+};
+
+window.addEventListener('scroll', function(){
+    BtnSetaCima();
 });
